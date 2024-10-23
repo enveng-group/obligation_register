2ca5df57eacf23a98cb7544dd1179b927b7baa57
enveng-group <164126503+enveng-group@users.noreply.github.com>
2024-10-23 11:47:19 +0800

    Refactor project structure and update paths
    - Deleted the 'server' binary file.
    - Modified 'src/main.c' to update the root directory path from '/home/agallo/obligation_register/' to '/home/ubuntu/obligation_register/'.
    - Minified 'styles/reset.css' by removing comments and unnecessary whitespace.

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@
 #include "../include/http.h"
 #include "../include/signal_handler.h"
 
-const char *root_directory = "/home/agallo/obligation_register/";
+const char *root_directory = "/home/ubuntu/obligation_register/";
 int server_fd;
 
 int
